26644eb114d7fe34de99c0c2d9e280e1885c1ef2
gemstoneyang <bennyws99@gmail.com>
2023-11-11 17:28:25 +0900

    feat: 주문 메뉴 입력 받기 기능 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -11,6 +11,13 @@ public class InputView {
         return input;
     }
 
+    public static String getOrder() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String input = getUserInput();
+
+        return input;
+    }
+
     private static String getUserInput() {
         return Console.readLine();
     }
