570c316a7a67f76c966e5d4f2436884170e4bbe0
gemstoneyang <bennyws99@gmail.com>
2023-11-15 14:24:36 +0900

    refactor: initService 메서드 수정

--- src/main/java/christmas/service/PlannerInitService.java
+++ src/main/java/christmas/service/PlannerInitService.java
@@ -3,41 +3,22 @@ package christmas.service;
 import christmas.domain.Date;
 import christmas.domain.Order;
 import christmas.domain.OrderHistory;
-import christmas.view.InputView;
 
 public class PlannerInitService {
 
-    public Order getOrder() {
-        Date date = getUserExpectDate();
-        OrderHistory orderHistory = getUserOrder();
+    public Order getOrder(Date date, OrderHistory orderHistory) {
         return Order.of(date, orderHistory);
     }
 
-    private Date getUserExpectDate() {
-        while (true) {
-            try {
-                String input = InputView.getExpectDate();
+    public Date getDate(String input) {
+        // todo: 검증 로직 추가
+        int day = Integer.parseInt(input);
 
-                // todo: 검증 로직 추가
-                int day = Integer.parseInt(input);
-
-                return Date.of(2023, 12, day);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+        return Date.of(2023, 12, day);
     }
 
-    private OrderHistory getUserOrder() {
-        while (true) {
-            try {
-                String input = InputView.getOrder();
-
-                return new OrderHistory(input);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+    public OrderHistory getOrderHistory(String input) {
+        // todo: 검증 로직 추가
+        return new OrderHistory(input);
     }
-
 }
