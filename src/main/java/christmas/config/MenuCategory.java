6ff8c30e6463bcbf93279d2c4fb59de66779e3d8
gemstoneyang <bennyws99@gmail.com>
2023-11-15 19:04:36 +0900

    refactor: 사용하지 않는 카테고리명 삭제

--- src/main/java/christmas/config/MenuCategory.java
+++ src/main/java/christmas/config/MenuCategory.java
@@ -4,11 +4,8 @@ package christmas.config;
  * 메뉴의 카테고리 정보를 담고 있는 Enum
  */
 public enum MenuCategory {
-    APPETIZER("애피타이저"),
-    MAIN("메인"),
-    DESSERT("디저트"),
-    DRINK("음료");
-
-    MenuCategory(String category) {
-    }
+    APPETIZER,
+    MAIN,
+    DESSERT,
+    DRINK
 }
